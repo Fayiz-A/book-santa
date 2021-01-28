88573b2826eccdc516dee0d25d3daa87bb91b17a
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-01-28 15:06:41 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import AppHeader from './components/AppBar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      dimensions: Dimensions.get('window')
+    }
+  }
+
+  componentDidMount() {
+    Dimensions.addEventListener("change", ({ window, screen }) => {
+      this.setState({
+        dimensions: window
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", () => console.log(`event listener removed in App.js`))
+  }
+
+  render() {
+    console.log(`Dimensions: ${dimensions}`)
+    let dimensions = this.state.dimensions;
+
+    return (
+      <View>
+        <AppHeader title='Book Santa'/>
+        <View>
+          <View style={styles(dimensions).emailTextInputContainer}>
+            <TextInput 
+              style={styles(dimensions).textInput}
+              onChangeText={(text) => console.log(text)}
+              placeholder='Email'
+            />
+          </View>
+          <View>
+            <TextInput />
+          </View>
+        </View>
+      </View>
+    );
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = (dimensions) => StyleSheet.create({
+  emailTextInputContainer: {
+    paddingTop: (dimensions.height / 2) - ((dimensions.height / 10) / 2),
+    paddingLeft: (dimensions.width / 2) - ((dimensions.width / 3) /  2)
   },
+  textInput: {
+    width: dimensions.width / 3,
+    height: dimensions.height / 15,
+    borderWidth: 4,
+    borderColor: 'black',
+    borderRadius: 10
+  }
 });
---
+++ components/AppBar.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textStyle}>{this.props.title}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  container: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2.0,    
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    backgroundColor: "cyan"
+  },
+});
+
+export default AppHeader;
